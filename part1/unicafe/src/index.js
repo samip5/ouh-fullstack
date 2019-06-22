c8259834f3fd1a0ae0c609769b5f8533f893c8be
Sami Mäntysaari <samip5@users.noreply.github.com>
2019-06-22 18:20:54 +0300

    1.6: Unicafe step1, done

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,15 +1,49 @@
 import React, { useState} from 'react'
 import ReactDOM from 'react-dom'
 
+const Display = props => <div>{props.name}: {props.value}</div>;
+
+const Button = (props) => (
+    <button onClick={props.handleClick}>
+        {props.text}
+    </button>
+);
+
 const App = () => {
-    // save clicks of each button to it's own state
     const [good, setGood] = useState(0);
     const [neutral, setNeutral] = useState(0);
     const [bad, setBad] = useState(0);
 
+    const handleGood = () => {
+        console.log('Good button clicked and added.');
+        setGood(good +1);
+    };
+
+    const handleNeutral = () => {
+        console.log('Neutral button clicked and added.');
+        setNeutral(neutral + 1);
+    };
+
+    const handleBad = () => {
+        console.log('Bad button clicked and added.');
+        setBad(bad + 1);
+    };
+
     return (
         <div>
-
+            <div>
+                <h2>Give some feedback</h2>
+                <Button handleClick={handleGood} text={"Good"}/>
+                <Button handleClick={handleNeutral} text={"Neutral"}/>
+                <Button handleClick={handleBad} text={"Bad"}/>
+                <br/>
+            </div>
+            <div>
+                <h2>Results</h2>
+            <Display name="Good" value={good}/>
+            <Display name="Neutral" value={neutral}/>
+            <Display name="Bad" value={bad}/>
+            </div>
         </div>
     )
 
