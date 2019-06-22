52b490b44fd9998ae510820d6551392f02eec80e
Sami Mäntysaari <sm@samip.fi>
2019-06-22 14:16:19 +0300

    1.6: Unicafe, step1 - Update

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -2,9 +2,10 @@ import React, { useState} from 'react'
 import ReactDOM from 'react-dom'
 
 const App = () => {
-    const [good, setGood] = useState(0)
-    const [neutral, setNeutral] = useState(0)
-    const [bad, setBad] = useState(0)
+    // save clicks of each button to it's own state
+    const [good, setGood] = useState(0);
+    const [neutral, setNeutral] = useState(0);
+    const [bad, setBad] = useState(0);
 
     return (
         <div>
